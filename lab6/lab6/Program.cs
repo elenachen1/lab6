549700155b80fa4078af9a1e9f4deaa2c0cf6a49
Lena <elenachen624>
2020-12-04 21:57:14 +0300

    2

--- lab6/lab6/Program.cs
+++ lab6/lab6/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace lab6
 {
@@ -6,7 +9,17 @@ namespace lab6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Work w1 = new Work("Печать пылеотвода", 1000);
+            Work w2 = new Work("Печать турбины", 500);
+            Work w3 = new Work("Печать корабля", 5000);
+
+            Work w11 = new Work("Создание прототапа робота", 10000);
+            Work w12 = new Work("Создание печатной платы(драйвер для моторов)", 5000);
+            Work w13 = new Work("Сборка 3д принтера", 8000);
+
+            List<Work> report = new List<Work>() { w1, w2, w3, w11, w12, w13};
+            List<Work> report_2 = report.Where(w => w.price == 5000).ToList();
+            List<Work> report_3 = report.OrderBy(w => w.name).ToList();
         }
     }
 }
---
+++ lab6/lab6/Work.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab6
+{
+    class Work
+    {
+        public string name;
+        public int price;
+
+        public Work(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
